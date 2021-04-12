423626a7c582589c59a932863135e7210a94de15
Ananthu Sudhikumar <45742784+amouxkin@users.noreply.github.com>
2021-04-12 18:57:25 +0530

    Merge pull request #6 from LedgerSync/feat/github-actions-update
    Feat/GitHub actions update

--- lib/ledger_sync/template_ledger/version.rb
+++ lib/ledger_sync/template_ledger/version.rb
@@ -5,13 +5,11 @@ module LedgerSync
   module TemplateLedger
     VERSION = '0.1.1'
 
-    def self.version(args = {})
-      pre = args.fetch(:pre, false)
-
-      if !pre && (!ENV['TRAVIS'] || ENV.fetch('TRAVIS_TAG', '') != '')
-        VERSION
+    def self.version
+      if ENV['PRE_RELEASE']
+        "#{VERSION}.pre.#{ENV['GITHUB_RUN_NUMBER']}"
       else
-        "#{VERSION}.pre.#{ENV['TRAVIS_BUILD_NUMBER']}"
+        VERSION
       end
     end
   end
