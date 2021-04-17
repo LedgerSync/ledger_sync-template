c1b4f2726b45686e777d18f52490345da10a7a5c
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2021-04-17 06:08:31 +0530

    refactor(test): module format

--- spec/sample_resource/find_spec.rb
+++ spec/sample_resource/find_spec.rb
@@ -4,6 +4,14 @@ require 'spec_helper'
 
 support :template_ledger_shared_examples
 
-RSpec.describe LedgerSync::TemplateLedger::SampleResource::Operations::Find, operation: true do
-  it_behaves_like 'a template_ledger operation'
+module LedgerSync
+  module TemplateLedger
+    class SampleResource
+      module Operations
+        RSpec.describe Find, operation: true do
+          it_behaves_like 'a template_ledger operation'
+        end
+      end
+    end
+  end
 end
